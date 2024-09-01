26c7a9b4bb5f6eeae59469b89271dcfbde427938
Laraib007 <lrbkhan01@gmail.com>
2024-09-01 14:00:38 +0500

    fixing

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const mongooseURI = 'mongodb+srv://mylaptop2334:Xg5jMIzojVIq2sUh@cluster0.axpbaoh.mongodb.net/inotebook';
-
-
-const connectToMongose = ()=>{
-    mongoose.connect(mongooseURI, console.log("database connected sucessfully"))
-}
-module.exports = connectToMongose;
